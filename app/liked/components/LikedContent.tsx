a3f62124230126979e080e174b1e79b236df18bc
Manasi Shah <manasishah@Manasis-MacBook-Air.local>
2023-09-05 11:40:01 +0530

    search and liked songs

---
+++ app/liked/components/LikedContent.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { Song } from "@/types";
+import { useUser } from "@/hooks/useUser";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+// import useOnPlay from "@/hooks/useOnPlay";
+
+interface LikedContentProps {
+  songs: Song[];
+}
+
+function LikedContent(props: LikedContentProps) {
+  const { songs } = props;
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No liked songs.
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center w-full gap-x-4">
+          <div className="flex-1">
+            <MediaItem data={song} onClick={() => {}} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default LikedContent;
